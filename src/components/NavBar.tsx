cc79de3fbcc1a55198c1103109023cd5e13c8c2c
Paulo José <paulocardosob@gmail.com>
2019-11-12 20:36:27 -0300

    Create NavBar.tsx

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBar: React.FC = () => {
+  return (
+    <div>
+      <p>Outros usuários</p>
+      <p>CineHub</p>
+      <p>Meus dados</p>
+    </div>
+  )
+}
